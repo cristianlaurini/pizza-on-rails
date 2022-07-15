0d959117c4c24895c9d17d57d7659c8e5a874101
Cristian Laurini <cristian.laurini@gmail.com>
2022-07-15 11:54:33 +0200

    installed annotate and graphql gems

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: customers
+#
+#  id         :integer          not null, primary key
+#  first_name :string           not null
+#  last_name  :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Customer < ApplicationRecord
     validates :first_name, presence: true
     validates :last_name, presence: true
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: customers
+#
+#  id         :integer          not null, primary key
+#  first_name :string           not null
+#  last_name  :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
